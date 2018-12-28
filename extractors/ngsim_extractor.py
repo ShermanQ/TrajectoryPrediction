21a7519cf162d2412d6b3e283ff56060c4e774aa
4if2016 <4if2016@gmail.com>
2018-12-28 08:32:06 -0500

    ngsim extractor naive

--- extractors/ngsim_extractor.py
+++ extractors/ngsim_extractor.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import csv
+import os
+import time
 
 DATASETS = "./datasets/"
 DATASET = "ngsim"
@@ -20,7 +22,16 @@ def get_scene_names(filename):
     return [s[:-1] for s in scene_names[1:]]
 
 def main():
-    # scene_names = get_scene_names(DATAFILE)
+
+    print("checking the existence of destination files...")
+    dict_type = {1:"motorcycle", 2:"car", 3:"truck"}
+    scene_names = get_scene_names(DATAFILE)
+
+    for scene_name in scene_names:
+        scene_file = CSV + scene_name + ".csv"
+        if os.path.exists(scene_file):
+            os.remove(scene_file)
+    print("Done!")
     # print(scene_names)
   
     # for scene_name in scene_names:
@@ -29,17 +40,57 @@ def main():
 
         # with open(scene_file,"a") as scene_csv:
         #     scene_writer = csv.writer(scene_csv)
+    print("processing extraction...")
 
-    with open(DATAFILE) as csv_reader:
-        line_num = 0
-        for line in csv_reader:
-            if line_num != 0:
-                line = line.split(",")
-                scene_name = line[-1][:-1]
-                scene_file = CSV + scene_name + ".csv"
+    # print_every = 3000000
+    start = time.time()
+    
+        
+    for scene_name in scene_names:
+        print("processing scene: " + scene_name)
+        with open(DATAFILE) as csv_reader:
+   
+            
+            scene_file = CSV + scene_name + ".csv"
 
-                print(scene_file)
-            line_num += 1
+            with open(scene_file,"a") as scene_csv:
+                scene_writer = csv.writer(scene_csv)
+
+                line_num = 0
+
+                for line in csv_reader:
+                    # if line_num % print_every == 0:
+                    #     print(line_num)
+                    #     print(time.time()-start)
+
+                    if line_num != 0:
+                        line = line.split(",")
+                        scene_line = line[-1][:-1]
+
+                        if scene_name == scene_line:
+
+
+                            row = []
+                            row.append(DATASET) #dataset
+                            row.append(scene_name) #scene
+                            row.append(int(line[1])) # frame
+                            row.append(int(line[0])) # id
+                            row.append(float(line[4])) #x
+                            row.append(float(line[5])) #y
+
+                            row.append(float(0)) #xl
+                            row.append(float(0)) #yl
+                            row.append(float(0)) #xb
+                            row.append(float(0)) #yb
+
+                            row.append(dict_type[int(line[10])]) # type
+
+                            scene_writer.writerow(row)
+
+
+
+                    line_num += 1
+    print(time.time()-start)
     
 
 
