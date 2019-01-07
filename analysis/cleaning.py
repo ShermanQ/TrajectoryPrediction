3488ce10bb64cc664260d5b35d13203cfa85dc17
4if2016 <4if2016@gmail.com>
2019-01-07 14:28:33 +0100

    comments for helpers_a.py

--- analysis/cleaning.py
+++ analysis/cleaning.py
@@ -4,9 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from extractors.helpers import get_dir_names,extract_frames,bb_intersection_over_union,extract_trajectories
 
 import time
-import json
-import numpy as np
-from scipy.spatial.distance import euclidean
+
 
 import helpers_a
 
@@ -29,7 +27,7 @@ def main():
     # csv = CSV + "fsc_0.csv"
     # for csv in csvs:
     #     print(csv)
-    deltas,lengths,outliers,props,missing_segments,nb_missing = helpers_a.trajectories_continuity(csv,temp_path = "./temp.txt")
+    # deltas,lengths,outliers,props,missing_segments,nb_missing = helpers_a.trajectories_continuity(csv,temp_path = "./temp.txt")
 
     # print(missing_segments)
     # for key in nb_missing:
@@ -38,7 +36,7 @@ def main():
     # print(outliers)
     # for csv in csvs:
     #     print(csv)
-    # nb_collisions_total,nb_objects_total,ious_total,ious_conflict_total = helpers_a.collisions_in_scene(csv) 
+    conflicts,nb_collisions_total,nb_objects_total,ious_total,ious_conflict_total = helpers_a.collisions_in_scene(csv) 
     #     print(time.time() - s)               
     # print(time.time() - s)
     # missing_values_multiple(csvs)
--- analysis/helpers_a.py
+++ analysis/helpers_a.py
@@ -9,7 +9,12 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 
 
-
+"""
+    Input: Extracted csv file of a dataset
+    Output: Number of line containing missing value, 
+            array containing number of missing value per feature
+            array of the lines containing missing values
+"""
 def missing_values(file_path,nb_features = 11):
 
     nb_missing = 0
@@ -36,6 +41,10 @@ def missing_values(file_path,nb_features = 11):
             
     return nb_missing,nb_missing_features,lines
 
+"""
+    Calls missing_values for a list of csv file
+"""
+
 def missing_values_multiple(file_pathes,nb_features = 11,func = missing_values):
 
 
@@ -48,6 +57,18 @@ def missing_values_multiple(file_pathes,nb_features = 11,func = missing_values):
 
 
 
+
+"""
+    For a frame (dictionnary representation)
+    Computes the pairwise iou between every object in the scene 
+    Detects conflict if iou is above a defined threshold
+    Output:
+        list of conflicts
+        number of collisions in the frame
+        number of object in the frame
+        pairwise ious between all object in the frame
+        pairwise ious between conflicted objects in the frame
+"""
 def collisions_in_frame(frame,threshold = 0.1):
 
     conflicts = []
@@ -81,6 +102,17 @@ def collisions_in_frame(frame,threshold = 0.1):
     return conflicts,nb_collisions,frame["frame"],nb_objects,ious,ious_conflict
 
 
+
+"""
+    Input: file_path of the scene
+    Calls collision in frame for every frame in the scene
+    Output:
+        Total number of collisions for the scene
+        Total number of objects for the scene
+        Every pairwise iou
+        Pairwise iou for conflicted agents
+        list of conflict for each frame
+"""
 def collisions_in_scene(file_path,temp_path = "./temp.txt"):
     # to be called in notebook with dataframe describe for further analysis
     extract_frames(file_path,temp_path,save=True)
@@ -88,6 +120,7 @@ def collisions_in_scene(file_path,temp_path = "./temp.txt"):
     nb_objects_total = []
     ious_total = []
     ious_conflict_total = []
+    conflicts_total = {}
     with open(temp_path) as frames:
         for frame in frames:
             frame = json.loads(frame)
@@ -100,15 +133,28 @@ def collisions_in_scene(file_path,temp_path = "./temp.txt"):
                 ious_total.append(iou)
             for iou in ious_conflict:
                 ious_conflict_total.append(iou)
+            conflicts_total[frame_id] = conflicts
     os.remove(temp_path)
 
-    return nb_collisions_total,nb_objects_total,ious_total,ious_conflict_total
+    return conflicts_total,nb_collisions_total,nb_objects_total,ious_total,ious_conflict_total
 
     # print(np.mean(nb_collisions_total),np.std(nb_collisions_total))
     # print(np.mean(nb_objects_total),np.std(nb_objects_total))
     # print(np.mean(ious_total),np.std(ious_total))
     # print(np.mean(ious_conflict_total),np.std(ious_conflict_total))
 
+
+
+"""
+    input: scene filepath
+    output:
+        dictionnary: key: trajectory_id, value: deltas
+        dictionnary: key: trajectory_id, value: length of trajectory
+        dictionnary: key: trajectory_id, value: list of outlier points
+        dictionnary: key: trajectory_id, value: proportion of outlier points
+        dictionnary: key: trajectory_id, value: list of missing segment
+        dictionnary: key: trajectory_id, value: number of missing segment
+"""
 def trajectories_continuity(file_path,temp_path = "./temp.txt"):
     # to be called in notebook with dataframe describe for further analysis
     extract_trajectories(file_path,temp_path, save = True)
@@ -137,6 +183,15 @@ def trajectories_continuity(file_path,temp_path = "./temp.txt"):
     os.remove(temp_path)
     return trajectories_deltas,trajectories_length,trajectories_outliers,trajectories_outliers_prop,trajectories_missing_segment,trajectories_nb_missing_segment
 
+
+"""
+    Input: 1D list of values to check
+    Do: Check wether a value is above 3 times the std of the list
+        If this is the case it's considered an outlier
+    Output: 
+        list of outlier points in the given list
+        Proportion of outlier in the list
+"""
 def three_sigma(values):
     sigma = np.std(values)
     outliers = []
@@ -145,6 +200,10 @@ def three_sigma(values):
             outliers.append((i-1,i))
     outliers_prop = float(len(outliers))/float(len(values)+1)
     return outliers, outliers_prop
+
+"""
+    Computes the distance between every pair of consecutive point in the trajectory
+"""
 
 def trajectory_deltas(trajectory,threshold = 0.1):
 
@@ -155,6 +214,10 @@ def trajectory_deltas(trajectory,threshold = 0.1):
         deltas.append(delta)
 
     return deltas
+
+"""
+    For a given trajectory, check wether there are missing frames
+"""
 
 def trajectory_frames(trajectory):
     frames = trajectory["frames"]
