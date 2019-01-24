17d386f0d0bc29135765d283469308c17d14064b
4if2016 <4if2016@gmail.com>
2019-01-24 11:17:30 -0500

    visualize cleaned plot_street

--- analysis/visualize.py
+++ analysis/visualize.py
@@ -23,7 +23,25 @@ import pandas as pd
 
 from matplotlib.lines import Line2D      
 from matplotlib.patches import Polygon
-
+def plot_street(param,depth):
+    lines = [
+        plt.Polygon([[-param,-param],[0,-param],[0,0],[-param,0]],color = "black",fill = False), #5
+        plt.Polygon([[0,0],[param,0],[param,-param],[0,-param]],color = "black",fill = False),#7
+        plt.Polygon([[0,0],[param,0],[param,param],[0,param]],color = "black",fill = False),#8
+        plt.Polygon([[0,0],[0,param],[-param,param],[-param,0]],color = "black",fill = False),#6
+        plt.Polygon([[-depth,-param],[-param,-param],[-param,0],[-depth,0]],color = "black",fill = False,closed = False),#4
+        plt.Polygon([[-depth,0],[-param,0],[-param,param],[-depth,param]],color = "black",fill = False,closed = False),#12
+        plt.Polygon([[-param,-depth],[-param,-param],[0,-param],[0,-depth]],color = "black",fill = False,closed = False),#9
+        plt.Polygon([[0,-depth],[0,-param],[param,-param],[param,-depth]],color = "black",fill = False,closed = False),#1
+        plt.Polygon([[depth,param],[param,param],[param,0],[depth,0]],color = "black",fill = False,closed = False),#3
+        plt.Polygon([[depth,0],[param,0],[param,-param],[depth,-param]],color = "black",fill = False,closed = False),#10
+        plt.Polygon([[-param,depth],[-param,param],[0,param],[0,depth]],color = "black",fill = False,closed = False),#2
+        plt.Polygon([[0,depth],[0,param],[param,param],[param,depth]],color = "black",fill = False,closed = False) #11
+    ]
+    for line in lines:
+        plt.gca().add_line(line)
+ 
+    
 
 def plot_trajectories(file_path, user_type = None,temp_path = "./temp.txt"):
     extract_trajectories(file_path,destination_path = temp_path, save = True)
@@ -32,104 +50,8 @@ def plot_trajectories(file_path, user_type = None,temp_path = "./temp.txt"):
     depth = 20
 
     #5
-    line = plt.Polygon([
-        [-param,-param],
-        [0,-param],
-        [0,0],
-        [-param,0]
-    ],color = "black",fill = False)
-    plt.gca().add_line(line)
-    #7
-    line = plt.Polygon([
-        [0,0],
-        [param,0],
-        [param,-param],
-        [0,-param]
-    ],color = "black",fill = False)
-    plt.gca().add_line(line)
-    #8
-    line = plt.Polygon([
-        [0,0],
-        [param,0],
-        [param,param],
-        [0,param]
-    ],color = "black",fill = False)
-    plt.gca().add_line(line)
-    #6
-    line = plt.Polygon([
-        [0,0],
-        [0,param],
-        [-param,param],
-        [-param,0]
-    ],color = "black",fill = False)
-    plt.gca().add_line(line)
-    #4
-    line = plt.Polygon([
-        [-depth,-param],
-        [-param,-param],
-        [-param,0],
-        [-depth,0]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-    #12
-    line = plt.Polygon([
-        [-depth,0],
-        [-param,0],
-        [-param,param],
-        [-depth,param]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-    #9
-    line = plt.Polygon([
-        [-param,-depth],
-        [-param,-param],
-        [0,-param],
-        [0,-depth]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-    #1
-    line = plt.Polygon([
-        [0,-depth],
-        [0,-param],
-        [param,-param],
-        [param,-depth]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-
-    #3
-    line = plt.Polygon([
-        [depth,param],
-        [param,param],
-        [param,0],
-        [depth,0]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-    #10
-    line = plt.Polygon([
-        [depth,0],
-        [param,0],
-        [param,-param],
-        [depth,-param]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-
-    #2
-    line = plt.Polygon([
-        [-param,depth],
-        [-param,param],
-        [0,param],
-        [0,depth]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-    #11
-    line = plt.Polygon([
-        [0,depth],
-        [0,param],
-        [param,param],
-        [param,depth]
-    ],color = "black",fill = False,closed = False)
-    plt.gca().add_line(line)
-
+    
+    plot_street(param,depth)
     with open(temp_path) as trajectories:
         # ids = [4,5]
         # ids = [0,1,2,3,4,5,6]
@@ -139,22 +61,24 @@ def plot_trajectories(file_path, user_type = None,temp_path = "./temp.txt"):
             coordinates = trajectory["coordinates"]
             id_ = trajectory["id"]
             # if id_ in ids:
-            if user_type == None:
-                x = [p[0] for p in coordinates]
-                y = [p[1] for p in coordinates]
+            id_scene = 4
+            if trajectory["scene"] == "minute number: "+str(id_scene):
+                if user_type == None:
+                    x = [p[0] for p in coordinates]
+                    y = [p[1] for p in coordinates]
+
+                    
+                    plt.plot(x,y)
+                    # plt.show() 
+
+                elif trajectory["user_type"] == user_type:
 
                 
-                plt.plot(x,y)
-                # plt.show() 
+                    x = [p[0] for p in coordinates]
+                    y = [p[1] for p in coordinates]
 
-            elif trajectory["user_type"] == user_type:
-
-            
-                x = [p[0] for p in coordinates]
-                y = [p[1] for p in coordinates]
-
-                
-                plt.plot(x,y)
+                    
+                    plt.plot(x,y)
         plt.show()    
         
         # print(i)
