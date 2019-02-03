c399f97ea0a8c091b53969288435caad27b61935
4if2016 <4if2016@gmail.com>
2019-02-03 18:11:49 -0500

    preprocessing display outliers

--- preprocessing.py
+++ preprocessing.py
@@ -7,12 +7,18 @@ from scipy.spatial import distance
 def plot_coordinates(coordinates):
     x = [p[0] for p in coordinates]
     y = [p[1] for p in coordinates]
-    plt.plot(x,y)   
+    plt.plot(x,y)  
+
+def plot_points(coordinates):
+    x = [p[0] for p in coordinates]
+    y = [p[1] for p in coordinates]
+    plt.scatter(x,y) 
 
 def display_trajectory(trajectory):
     coordinates = trajectory["coordinates"]
     plot_coordinates(coordinates)
-    plt.show()
+    plot_points(coordinates)
+   
 
 def get_speed(point1,point2,deltat):
     d = distance.euclidean(point1,point2)
@@ -25,17 +31,41 @@ def get_speeds(coordinates,framerate):
         speeds.append(speed)
     return speeds
 
+def get_acceleration(v1,v2,deltat):
+    a = (v2-v1)/deltat
+    return a
+
+def get_accelerations(speeds,framerate):
+    accelerations = []
+    for i in range(1,len(speeds)):
+        acceleration = get_acceleration(speeds[i-1],speeds[i],framerate)
+        accelerations.append(acceleration)
+    return accelerations
+
 def find_outlier_points(trajectory,framerate = 0.1,acceleration_thresh = 5, deceleration_thresh = -8):
     coordinates = trajectory["coordinates"]
     speeds = get_speeds(coordinates,framerate)
-    accelerations = get_speeds(coordinates,framerate)
+    speeds = [speeds[0]] + speeds 
+    accelerations = get_accelerations(speeds,framerate)
 
     counter = 0
-    for a in accelerations:
+    indices = []
+    outlier_accelerations = []
+    for i,a in enumerate(accelerations):
         if not( a > deceleration_thresh and a < acceleration_thresh):
+            outlier_accelerations.append(a)
             counter += 1
-    return counter
+            if i not in indices:
+                indices.append(i)
+            if i + 1 not in indices:
+                indices.append(i+1)
+    outlier_points = [coordinates[i] for i in indices]
     
+
+    return counter,outlier_points,outlier_accelerations
+    
+ # # # #  # # # # # #
+  # # # #  # # # # #
 def main():
     filepath = "./data/csv/lankershim_inter2.csv"
     temp_path = "./data/temp/temp.txt"
@@ -46,12 +76,14 @@ def main():
             for i,trajectory in enumerate(trajectories):
                 trajectory = json.loads(trajectory)
 
-                nb_outliers = find_outlier_points(trajectory)
+                nb_outliers,outlier_points,outlier_accelerations = find_outlier_points(trajectory)
                 if nb_outliers > 0:
                     print(i)
                     print(nb_outliers)
-
+                    print(outlier_accelerations)
                     display_trajectory(trajectory)
+                    plot_points(outlier_points)
+                    plt.show()
     except:
         os.remove(temp_path)  
 if __name__ == "__main__":
