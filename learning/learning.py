45ec1eaa6c966136f8ce8ec0d3d1eca2fc97741f
4if2016 <4if2016@gmail.com>
2019-02-12 10:06:43 -0500

    learning seq2seq class

--- learning/learning.py
+++ learning/learning.py
@@ -37,8 +37,8 @@ class encoderLSTM(nn.Module):
     def forward(self,x):
         self.hidden = self.init_hidden_state()
         output,self.hidden = self.lstm(x,self.hidden)
-        decoder_start = self.last_output(output[0])
-        return decoder_start, self.hidden
+        decoder_start = self.last_output(output[:,-1])
+        return decoder_start.view(self.batch_size,1,self.input_size), self.hidden
 
     def init_hidden_state(self):
         h_0 = torch.zeros(self.num_layers,self.batch_size,self.hidden_size).cuda()
@@ -49,7 +49,7 @@ class encoderLSTM(nn.Module):
 class decoderLSTM(nn.Module):
     # def __init__(self,input_size,hidden_size,num_layers,output_size,batch_size,seq_len):
     
-    def __init__(self,output_size,hidden_size,num_layers,batch_size,seq_len,teacher_forcing = False):
+    def __init__(self,output_size,hidden_size,num_layers,batch_size):
         super(decoderLSTM,self).__init__()
 
         self.hidden_size = hidden_size
@@ -57,19 +57,18 @@ class decoderLSTM(nn.Module):
         self.output_size = output_size
         self.batch_size = batch_size
         self.hidden = self.init_hidden_state()
-        self.seq_len = seq_len
-        self.teacher_forcing = teacher_forcing
+        # self.seq_len = seq_len
         # self.target = target
 
         
-        self.lstm = nn.LSTM(input_size = self.hidden_size,hidden_size = self.hidden_size,num_layers = self.num_layers)
+        self.lstm = nn.LSTM(input_size = self.output_size,hidden_size = self.hidden_size,num_layers = self.num_layers,batch_first = True)
         self.out = nn.Linear(self.hidden_size,self.output_size)
 
         
 
     def forward(self,x,hidden):
         # self.hidden = self.init_hidden_state()
-        x = x.view(self.seq_len,self.batch_size,2)
+        # x = x.view(self.seq_len,self.batch_size,2)
         output,self.hidden = self.lstm(x,hidden)
         output = self.out(output)
         return output, hidden
@@ -80,7 +79,29 @@ class decoderLSTM(nn.Module):
 
         return (h_0,c_0)
 
+class seq2seq(nn.Module):
+    def __init__(self,input_size,output_size,hidden_size,hidden_size_d,num_layers,num_layers_d,batch_size,seq_len_d,teacher_forcing = False):
+        super(seq2seq,self).__init__()
+        self.encoder = encoderLSTM(input_size,hidden_size,num_layers,batch_size)
+        self.decoder = decoderLSTM(output_size,hidden_size_d,num_layers_d,batch_size)
+        self.seq_len_d = seq_len_d
+        self.batch_size = batch_size
+        self.output_size = output_size
 
+    def forward(self,x,y):
+        encoder_output, encoder_state = self.encoder(x)
+
+    
+        output,state = encoder_output,encoder_state
+        outputs = []
+        for _ in range(self.seq_len_d):
+            output,state = self.decoder(output,state)
+            outputs.append(output)
+        outputs = torch.stack(outputs)
+        return outputs.view(self.batch_size,self.seq_len_d,self.output_size)
+
+
+        
 
 class MLP(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
@@ -195,13 +216,17 @@ def main():
     print(device)
     # input_size,hidden_size,output_size = 16,1000,24
     # net = MLP(input_size,hidden_size,output_size)
-    input_size,hidden_size,num_layers,output_size,batch_size,seq_len  = 2,30,1,2,200,8
+    input_size,hidden_size,num_layers,output_size,batch_size,seq_len,seq_len_d  = 2,30,1,2,200,8,12
 
-    loader = torch.utils.data.DataLoader( dataset, batch_size= batch_size, shuffle=True,num_workers= 30,drop_last = True)
+    loader = torch.utils.data.DataLoader( dataset, batch_size= batch_size, shuffle=True,num_workers= 10,drop_last = True)
 
-    net = encoderLSTM(input_size,hidden_size,num_layers,batch_size,seq_len)
+    # net = encoderLSTM(input_size,hidden_size,num_layers,batch_size)
+    # net.to(device)
+    # decoder = decoderLSTM(output_size,hidden_size,num_layers,batch_size)
+    # decoder.to(device)
+
+    net = seq2seq(input_size,output_size,hidden_size,hidden_size,num_layers,num_layers,batch_size,seq_len_d)
     net.to(device)
-
     learning_rate = 0.001
     n_epochs = 1
     #loss
@@ -232,11 +257,17 @@ def main():
             optimizer.zero_grad()
 
             
-    
+            outputs = net(inputs,targets)
+            print(outputs)
+            print(len(outputs))
 
-            a,b = net(inputs)
-            c,d = b
-            print(c.size())
+            print(outputs.size())
+
+            print(targets.size())
+            # start,hidden_state = net(inputs)
+            # print(start.size())
+            # output, hidden = decoder(start,hidden_state)
+            # print(output.size())
             # loss = criterion(outputs,targets)
 
     #         # # del inputs
@@ -252,7 +283,7 @@ def main():
                 # print(loss.item()/200)
                 print(time.time()-s)
             break
-        break
+        break            
 
         # print(epoch_loss/i)
         # losses.append(epoch_loss)
