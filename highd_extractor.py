16dfac4edfd8d4514592cdc5c6a923839322eb70
4if2016 <4if2016@gmail.com>
2019-03-01 13:35:44 -0500

    highd class

--- highd_extractor.py
+++ highd_extractor.py
@@ -3,80 +3,89 @@ import os
 import time
 import extractors.helpers as helpers
 import numpy as np
+import sys
+import json
 
 
 
-DATASETS = "./data/datasets/"
-DATASET = "highd"
-DATAFILE = DATASETS + DATASET + "/" + DATASET + ".csv"
-CSV = "./data/csv/"
-MAIN = "main/"
-TRACKS = "/tracks/"
-META = "/tracks_meta/"
 
-def main():
-    csv_files = helpers.get_dir_names(DATASETS + DATASET+TRACKS)
-    csv_meta = helpers.get_dir_names(DATASETS + DATASET+META,lower = False)
-    print(csv_files)
-
-    # for csv_ in csv_files:
-    #     scene_file = CSV + csv_
-    #     # print(scene_file)
-    #     if os.path.exists(scene_file):
-    #         os.remove(scene_file)
-
-
-    dataset_file = CSV + MAIN + DATASET + ".csv"
-    if os.path.exists(dataset_file):
-        os.remove(dataset_file)
-    with open(dataset_file,"a") as dataset_csv:
+# TODO bounding box
+class HighdExtractor():
+    def __init__(self, data_path,param_path):
         
-        dataset_writer = csv.writer(dataset_csv)
+        'Initializing parameters'
+        data = json.load(open(data_path))
+        param = json.load(open(param_path))
 
+        self.dataset = param["dataset"]
+        self.original_dataset_tracks = data["original_datasets"] + param["tracks_path"]
+        self.original_dataset_meta = data["original_datasets"] + param["meta_path"]
+        self.original_scene_tracks = data["original_datasets"] + param["tracks_path"] +"{}"
+        self.original_scene_meta = data["original_datasets"] + param["meta_path"] +"{}"
+
+
+        self.destination_file = data["extracted_datasets"] + "{}"
+    
+    def extract(self):
+        csv_files = helpers.get_dir_names(self.original_dataset_tracks)
+        csv_meta = helpers.get_dir_names(self.original_dataset_meta,lower = False)
+            
         for csv_,meta in zip(csv_files,csv_meta):
             print("processing scene: " + csv_)
-            with open(DATASETS+DATASET+TRACKS + csv_) as csv_reader:
+            with open(self.original_scene_tracks.format(csv_) ) as csv_reader:
                 csv_reader = csv.reader(csv_reader, delimiter=',')
 
-                with open(DATASETS+DATASET+META + meta) as meta_reader:
+                with open(self.original_scene_meta.format(meta)) as meta_reader:
                     meta_reader = csv.reader(meta_reader, delimiter=',')
                 
-                    scene_file = CSV + csv_
-                    if os.path.exists(scene_file):
-                        os.remove(scene_file)
+                    scene_file = self.destination_file.format( csv_)
+                    helpers.remove_file(scene_file)
+
+
                     with open(scene_file,"a") as scene_csv:
                         subscene_writer = csv.writer(scene_csv)
 
-                        line1 = next(meta_reader)
+                        row_meta = next(meta_reader)
                         last_id = "-2"
-                        for i,line in enumerate(csv_reader):
+                        new_scene_name = csv_.split(".")[0]
+                        for i,row in enumerate(csv_reader):
                             if i != 0:
-                                new_id = line[1]
+                                new_id = row[1]
                                 if new_id != last_id:
                                     try:
-                                        line1 = next(meta_reader)
+                                        row_meta = next(meta_reader)
                                     except:
-                                        "no line available"
-                                new_scene_name = csv_.split(".")[0]
-                                unknown = -1
-                                row = []
-                                row.append(DATASET) #dataset
-                                row.append(new_scene_name) #scene
-                                row.append(int(line[0])) # frame
-                                row.append(int(line[1])) # id
-                                row.append(float(line[2])) #x
-                                row.append(float(line[3]))  #y
+                                        "no row available"
+                                
+                                new_row = self.__parse_row(row,row_meta,new_scene_name)
 
-                                row.append(unknown) #xl
-                                row.append(unknown) #yl
-                                row.append(unknown) #xb
-                                row.append(unknown) #yb
+                                subscene_writer.writerow(new_row)
+                                last_id = new_id
+    def __parse_row(self,row,row_meta,new_scene_name):
+        
+        unknown = -1
+        new_row = []
+        new_row.append(self.dataset) #dataset
+        new_row.append(new_scene_name) #scene
+        new_row.append(int(row[0])) # frame
+        new_row.append(int(row[1])) # id
+        new_row.append(float(row[2])) #x
+        new_row.append(float(row[3]))  #y
 
-                                row.append(line1[6].lower()) # type
+        new_row.append(unknown) #xl
+        new_row.append(unknown) #yl
+        new_row.append(unknown) #xb
+        new_row.append(unknown) #yb
 
-                                subscene_writer.writerow(row)
-                                dataset_writer.writerow(row)
-                    
+        new_row.append(row_meta[6].lower()) # type
+        return new_row
+        
+# python highd_extractor.py parameters/data.json parameters/highd_extractor.json
+
+def main():
+    args = sys.argv
+    highd_extractor = HighdExtractor(args[1],args[2])
+    highd_extractor.extract()
 
 if __name__ == "__main__":
     main()
