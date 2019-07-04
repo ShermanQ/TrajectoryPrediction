3d95eeee2244a77900965c182825caf12cb8c705
elbuco1 <4if2016@gmail.com>
2019-07-04 11:27:02 -0400

    mobilenet

--- learning/classes/pretrained_vgg.py
+++ learning/classes/pretrained_vgg.py
@@ -83,10 +83,10 @@ class customCNN1(nn.Module):
         super(customCNN1,self).__init__()
 
         # For mobilenet_v2 uncomment following
-        # self.cnn = torchvision.models.mobilenet_v2(pretrained=True).features #mobilenet
+        self.cnn = torchvision.models.mobilenet_v2(pretrained=True).features #mobilenet
         # self.cnn = torchvision.models.vgg19(pretrained=True).features #vgg19
 
-        self.cnn = torchvision.models.segmentation.fcn_resnet101(pretrained=True).backbone #semantic segmentation
+        # self.cnn = torchvision.models.segmentation.fcn_resnet101(pretrained=True).backbone #semantic segmentation
         # print(self.cnn)
 
         self.reduce_layer = nn.AdaptiveAvgPool2d((7,7))
@@ -97,16 +97,16 @@ class customCNN1(nn.Module):
  
     
     def forward(self,x):
-        # x = self.cnn(x)  
-        x = self.cnn(x)['out'][0] #semantic
+        x = self.cnn(x)  
+        # x = self.cnn(x)['out'][0] #semantic
 
         cnn_features = self.reduce_layer(x)
         return cnn_features
 
 class customCNN2(nn.Module):
-    # def __init__(self,device, nb_channels_out = 1280,nb_channels_projection = 128): #mobilenet
+    def __init__(self,device, nb_channels_out = 1280,nb_channels_projection = 128): #mobilenet
     # def __init__(self,device, nb_channels_out = 512,nb_channels_projection = 128): #vgg19
-    def __init__(self,device, nb_channels_out = 2048,nb_channels_projection = 128): #segmentation
+    # def __init__(self,device, nb_channels_out = 2048,nb_channels_projection = 128): #segmentation
 
         super(customCNN2,self).__init__()
         self.device = device
